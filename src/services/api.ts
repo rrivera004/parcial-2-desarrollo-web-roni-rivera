abd5c61f9e2b1c8df43c05c1479f896f285d8f16
rrivera004 <rriverav2@miumg.edu.gt>
2026-09-19 19:45:43 -0600

    Aumentar timeout de API

--- src/services/api.ts
+++ src/services/api.ts
@@ -10,7 +10,7 @@ export const apiClient = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  timeout: 10000,
+  timeout: 60000,
 });
 
 // Interceptor: adjunta el token JWT a cada petición
